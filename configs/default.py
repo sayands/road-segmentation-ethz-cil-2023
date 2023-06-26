a35854bda6325731e6bca4c66c3d92673e729db4
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-06-27 00:18:38 +0200

    Tested the full testing pipeline + mask outputs get resized to 400x400 using nearest neighbor interpolation

--- configs/default.py
+++ configs/default.py
@@ -60,6 +60,7 @@ _C.loss.gamma = 1.5
 # testing params
 _C.test = CN()
 _C.test.test_path = ''
+_C.test.mask_results_path = ''
 _C.test.submission_path = ''
 _C.test.model_path = ''
 _C.test.device = 'cpu'
--- src/test.py
+++ src/test.py
@@ -2,10 +2,16 @@ import argparse
 import logging
 import json
 import os
+import re
 
 import numpy as np
 import torch
 import cv2
+import PIL
+from PIL import Image as im
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 import segmentation_models_pytorch as smp
 
@@ -25,14 +31,15 @@ def patch_to_label(patch):
     else:
         return 0
 
-
-def mask_to_submission_strings(im_arr, mask_dir=None):
+def mask_to_submission_strings(image_filename, mask_dir=None):
     """Reads a single image and outputs the strings that should go into the submission file"""
-    img_number += 1
+    img_number = int(re.search(r"\d+", image_filename).group(0))
+    im = PIL.Image.open(image_filename)
+    im_arr = np.asarray(im)
     if len(im_arr.shape) > 2:
         # Convert to grayscale.
-        im_arr = im_arr.convert("L")
-        im_arr = np.asarray(im_arr)
+        im = im.convert("L")
+        im_arr = np.asarray(im)
 
     patch_size = 16
     mask = np.zeros_like(im_arr)
@@ -43,20 +50,18 @@ def mask_to_submission_strings(im_arr, mask_dir=None):
             mask[i:i+patch_size, j:j+patch_size] = int(label*255)
             yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
 
-    # if mask_dir:
-    #     save_mask_as_img(mask, os.path.join(mask_dir, "mask_" + image_filename.split("/")[-1]))
+#     if mask_dir:
+#         save_mask_as_img(mask, os.path.join(mask_dir, "mask_" + image_filename.split("/")[-1]))
+    
 
-# def save_mask_as_img(img_arr, mask_filename):
-#     img = PIL.Image.fromarray(img_arr)
-#     os.makedirs(os.path.dirname(mask_filename), exist_ok=True)
-#     img.save(mask_filename)
+def save_mask_as_img(img_arr, mask_filename):
+    img = PIL.Image.fromarray(img_arr)
 
-def masks_to_submission(submission_filename, mask_dir, *image_filenames):
-    """Converts images into a submission file"""
-    with open(submission_filename, 'w') as f:
-        f.write('id,prediction\n')
-        for fn in image_filenames[0:]:
-            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn, mask_dir=mask_dir))
+    # Temporarily resizing image to 400x400 using nearest neighbor interpolation
+    img = img.resize((400,400), im.NEAREST)
+
+    os.makedirs(os.path.dirname(mask_filename), exist_ok=True)
+    img.save(mask_filename)
 
 def test(config):
     # Load model
@@ -64,39 +69,48 @@ def test(config):
                               encoder_output_stride=16, decoder_channels=256, decoder_atrous_rates=(12, 24, 36), 
                               in_channels=3, classes=2, activation=None, upsampling=4, aux_params=None)
     
-    model = model.load_state_dict(torch.load(config["test"]["model_path"]))
+    model.load_state_dict((torch.load(config["test"]["model_path"]))['model'])
 
-    os.makedirs(os.path.dirname(config["test"]["submission_path"]), exist_ok=True)
-    with open(config["test"]["submission_path"], 'w') as f:
-        f.write('id, prediction\n')
+    # Load and evaluate images
+    for path in os.listdir(config["test"]["test_path"]):
+        im_path = os.path.join(os.path.abspath(config["test"]["test_path"]), path)
+        # Set model to evaluation
+        model.eval()
 
-        # Load and evaluate images
-        for path in os.listdir(config["test"]["test_path"]):
-            # Set model to evaluation
-            model.eval()
+        # No gradient tracking
+        with torch.no_grad():
+            # Load image
+            image = cv2.imread(im_path)
+            image = image[:, :, [2, 1, 0]] # Convert to RGB
+            image = image.astype(np.float32)
+            image /= 255.0
 
-            # No gradient tracking
-            with torch.no_grad():
-                # Load image
-                image = cv2.imread(path)
-                image = image[:, :, [2, 1, 0]] # Convert to RGB
-                image = image.astype(np.float32)
-                image /= 255.0
+            # Resize image
+            image = cv2.resize(image, (256, 256))
 
-                # Resize image
-                image = cv2.resize(image, (128, 128))
+            image = np.transpose(image, (2, 0, 1))
+            image = np.expand_dims(image, 0)
+            image = torch.from_numpy(image).to(config["test"]["device"])
 
-                image = np.transpose(image, (2, 0, 1))
-                image = np.expand_dims(image, 0)
-                image = torch.from_numpy(image).to(config["test"]["device"])
+            # make prediction
+            prediction_mask = model(image)
 
-                # make prediction
-                prediction_mask = model(image).squeeze()
-                prediction_mask = torch.sigmoid(prediction_mask)
-                prediction_mask = prediction_mask.cpu().numpy()
+            prediction_mask = prediction_mask[0].cpu().numpy()
+            prediction_mask = np.transpose(prediction_mask, (1, 2, 0))
+            prediction_mask = np.argmax(prediction_mask, axis=2)
+            prediction_mask *= 255
+            prediction_mask = prediction_mask.astype(np.uint8)
 
-                # Convert mask to submission
-                f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(prediction_mask))
+            pred_mask = np.reshape(prediction_mask, (256,256))
+            save_mask_as_img(pred_mask, os.path.join(config["test"]["mask_results_path"], "mask_" + im_path.split("/")[-1]))
+
+def masks_to_submission(submission_filename, mask_dir, *image_filenames):
+    os.makedirs(os.path.dirname(submission_filename), exist_ok=True)
+    """Converts images into a submission file"""
+    with open(submission_filename, 'w') as f:
+        f.write('id,prediction\n')
+        for fn in image_filenames[0:]:
+            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn, mask_dir=mask_dir))
 
 def parse_args():
     parser = argparse.ArgumentParser(description="CIL Project")
@@ -111,8 +125,14 @@ def parse_args():
 
 def main():
     parser, args = parse_args()
-    cfg = update_config(config, args.config)    
+    cfg = update_config(config, args.config)
+
+    # Runs the model over test set and saves the predicted mask images
     test(cfg)
+
+    # gets the predicted mask images and converts to submission
+    image_filenames = [os.path.join(config["test"]["mask_results_path"], name) for name in os.listdir(config["test"]["mask_results_path"])]
+    masks_to_submission(config["test"]["submission_path"], "", *image_filenames)
 
 if __name__ == '__main__':
     main()
